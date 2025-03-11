b240cc1da10946a1afc4ee0fb170e95e6f08f098
bovandenoever <as@#$kdwj394>
2025-03-11 15:19:12 +0100

    added form header + changed tax advisor content

--- components/sections/FormSection.tsx
+++ components/sections/FormSection.tsx
@@ -6,7 +6,7 @@ export default function FormSection({ formName, locale }: { formName: string, lo
     <div className="container mx-auto py-8">
       <div className="max-w-2xl mx-auto mb-8 text-center">
         <h2 className="text-3xl font-bold mb-4">{locale === "nl" ? "Deskundig belastingadvies op maat" : "Expert tax advice tailored to your situation"}</h2>
-        <p className="text-gray-600">{locale === "nl" ? "Heb je een complexe situatie die om specialistische kennis vraagt of wil je jouw belastingaangifte met vertrouwen uit handen geven? Vul het formulier in en ontvang passend advies van een belastingexpert die gespecialiseerd is in jouw situatie. Hoe meer informatie je deelt, hoe gerichter we je kunnen helpen, zonder onnodige vervolgvragen of vertraging." : "Do you have a complex situation that requires specialist knowledge or do you want to outsource your tax return with confidence? Fill out the form and receive tailored advice from a tax expert who is specialized in your situation. The more information you share, the more we can help you, without unnecessary follow-up questions or delays."}</p>
+        <p className="text-gray-600">{locale === "nl" ? "Heb je een complexe belastingaangifte of wil je dat een expert het voor je regelt? Vul het formulier in en krijg hulp van een belastingadviseur die jouw situatie begrijpt. Hoe meer informatie je deelt, hoe beter we je kunnen helpen zonder extra vragen of vertraging." : "Got a complex tax situation or want an expert to handle your tax return? Fill out the form and get help from a tax advisor who understands your needs. The more information you share, the better we can help you without extra questions or delays."}</p>
       </div>
       <DynamicForm handle={formName} />
     </div>
