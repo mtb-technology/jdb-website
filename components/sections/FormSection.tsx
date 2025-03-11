f2f5d314e45bc324f781e8526ba7d0f7a0572c33
Beaudinn Greve <beaudinngreve@gmail.com>
2025-03-11 21:57:49 +0000

    Uncommited changes

--- components/sections/FormSection.tsx
+++ components/sections/FormSection.tsx
@@ -8,7 +8,9 @@ export default function FormSection({ formName, locale }: { formName: string, lo
         <h2 className="text-3xl font-bold mb-4">{locale === "nl" ? "Deskundig belastingadvies op maat" : "Expert tax advice tailored to your situation"}</h2>
         <p className="text-gray-600">{locale === "nl" ? "Heb je een complexe belastingaangifte of wil je dat een expert het voor je regelt? Vul het formulier in en krijg hulp van een belastingadviseur die jouw situatie begrijpt. Hoe meer informatie je deelt, hoe beter we je kunnen helpen zonder extra vragen of vertraging." : "Got a complex tax situation or want an expert to handle your tax return? Fill out the form and get help from a tax advisor who understands your needs. The more information you share, the better we can help you without extra questions or delays."}</p>
       </div>
-      <DynamicForm handle={formName} />
+      <div className="py-22">
+        <DynamicForm handle={formName} />
+      </div>
     </div>
   );
 }
