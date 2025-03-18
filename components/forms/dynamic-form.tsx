942252637ec92825e869b31d2cecec7f9cdebc03
bovandenoever <as@#$kdwj394>
2025-03-18 17:52:21 +0100

    fix

--- components/forms/dynamic-form.tsx
+++
@@ -1,8 +0,0 @@
-                        <FormLabel>
-                          {locale === "en" ? (
-                            <>I agree to the <Link href="/en/privacy">privacy policy</Link> and consent to being contacted.</>
-                          ) : (
-                            <>Ik ga akkoord met de <Link href="/privacy">privacyvoorwaarden</Link> en geef toestemming om contact met mij op te nemen.</>
-                          )}
-                          <span className="text-destructive ml-1">*</span>
-                        </FormLabel> 
