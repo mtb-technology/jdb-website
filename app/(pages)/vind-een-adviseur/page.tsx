0c44409d86098fb8f5293f9b779da4a82b398673
bovandenoever <as@#$kdwj394>
2025-03-18 11:59:09 +0100

    new form functionality

--- app/(pages)/vind-een-adviseur/page.tsx
+++ app/(pages)/vind-een-adviseur/page.tsx
@@ -15,14 +15,12 @@ export default async function VindEenAdviseurPage({
 }: VindEenAdviseurPageProps) {
   const locale = params.locale || "nl";
   const dict = await getDictionary(locale);
-  //const howItWorks = dict.pages["how-it-works"] as HowItWorksDict;
 
   return (
     <main className="relative flex-1 flex flex-col pt-20">
       <Header dict={dict} />
       <div className="flex-1 overflow-y-auto">
         <div className="max-w-5xl mx-auto px-6 py-12">
-          {/* Find an Advisor Content */}
           <AdvisorFinder dict={dict.pages["find-advisor"]} locale={locale} />
         </div>
       </div>
